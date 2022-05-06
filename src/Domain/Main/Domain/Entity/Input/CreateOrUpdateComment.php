10ad5c35160e7bf58e5b918e6e420c40c753d19a
Boris Bobol <bobol.boris@gmail.com>
2022-05-06 15:18:32 +0300

    Create CreateOrUpdateComment

---
+++ src/Domain/Main/Domain/Entity/Input/CreateOrUpdateComment.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Domain\Main\Domain\Entity\Input;
+
+use App\Domain\Common\Domain\Entity\Base\Comment;
+use Symfony\Component\Validator\Constraints as Assert;
+use BBLDN\EntityExistsValidatorBundle\Doctrine\Validator as BBLDNValidator;
+
+#[BBLDNValidator\EntityExists(entityClass: Comment::class, fields: ["id"])]
+class CreateOrUpdateComment
+{
+    /* Идентификатор атрибута */
+    #[Assert\Sequentially([
+        new Assert\NotNull(),
+        new Assert\Type("integer"),
+    ])]
+    protected ?int $id = null;
+
+    /* Идентификатор языка */
+    #[Assert\Sequentially([
+        new Assert\NotNull(),
+        new Assert\Type("string"),
+        new Assert\Length(min: 1, max: 1024),
+    ])]
+    protected ?string $comment = null;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return CreateOrUpdateComment
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @param string|null $comment
+     * @return CreateOrUpdateComment
+     */
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
