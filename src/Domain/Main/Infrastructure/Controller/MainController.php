68670cef18d434e2aad9e369a43e82464aefd7dc
Boris Bobol <bobol.boris@gmail.com>
2022-05-07 12:14:15 +0300

    Create removeCommentByIdAction

--- src/Domain/Main/Infrastructure/Controller/MainController.php
+++ src/Domain/Main/Infrastructure/Controller/MainController.php
@@ -3,11 +3,13 @@
 namespace App\Domain\Main\Infrastructure\Controller;
 
 use BBLDN\CQRSBundle\QueryBus\QueryBus;
+use BBLDN\CQRSBundle\CommandBus\CommandBus;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Domain\Main\Application\Query\CommentList;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Domain\Common\Application\EntityToArrayHydrator\Hydrator as EntityToArrayHydrator;
+use \App\Domain\Main\Application\Command\RemoveCommentById;
 
 class MainController extends AbstractController
 {
@@ -27,6 +29,24 @@ class MainController extends AbstractController
 
         return $this->json([
             'data' => $entityToArrayHydrator->hydrateArray($commentList),
+        ]);
+    }
+
+    /**
+     * @param string $id
+     * @param CommandBus $commandBus
+     * @return JsonResponse
+     */
+    #[Route(path: "/comment/{id}", methods: "DELETE", requirements: ['id' => '\d+'])]
+    public function removeCommentById(
+        string $id,
+        CommandBus $commandBus
+    ): JsonResponse
+    {
+        $command = new RemoveCommentById((int)$id);
+
+        return $this->json([
+            'data' => $commandBus->execute($command),
         ]);
     }
 }
