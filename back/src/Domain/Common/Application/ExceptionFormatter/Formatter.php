7da7ff87bc6b3ff6b566fc191ee6710853ef5191
Boris Bobol <bobol.boris@gmail.com>
2022-05-09 12:30:44 +0300

    Create ExceptionFormatter

---
+++ back/src/Domain/Common/Application/ExceptionFormatter/Formatter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Domain\Common\Application\ExceptionFormatter;
+
+use Throwable;
+use Symfony\Component\HttpFoundation\Exception\JsonException;
+use Psr\Log\LoggerInterface as Logger;
+use App\Domain\Common\Domain\Exception\RestException;
+use App\Domain\Common\Domain\Exception\ValidateException;
+
+class Formatter
+{
+    private Logger $logger;
+
+    /**
+     * @param Logger $logger
+     */
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param Throwable $throwable
+     * @return array
+     *
+     * @psalm-return array{message: string, exception: string, errors?: mixed}
+     */
+    public function format(Throwable $throwable): array
+    {
+        if (true === is_a($throwable, ValidateException::class)) {
+            return [
+                'exception' => 'ValidateException',
+                'message' => $throwable->getMessage(),
+                'errors' => $throwable->getErrors(),
+            ];
+        }
+
+        if (true === is_a($throwable, RestException::class)) {
+            return [
+                'exception' => 'RestException',
+                'message' => $throwable->getMessage(),
+            ];
+        }
+
+        if (true === is_a($throwable, JsonException::class)) {
+            return [
+                'exception' => 'RestException',
+                'message' => 'Неверный запрос',
+            ];
+        }
+
+        $this->logger->error((string)$throwable);
+
+        return [
+            'exception' => 'RestException',
+            'message' => 'Неизвестная ошибка обратитесь к администратору',
+        ];
+    }
+}
